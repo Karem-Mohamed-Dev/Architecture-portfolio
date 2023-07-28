f1bd7447c92f50f7f24804f369d8d81ee5ecb691
Karim <karem109k@gmail.com>
2023-07-28 19:12:08 +0300

    close website

---
+++ src/closed.tsx
@@ -0,0 +1,13 @@
+const Closed = () => {
+    return (
+        <div style={{height: "100vh" ,padding: "2rem", textAlign: "center", gap: "2rem" ,fontSize: "2rem" ,display: "flex",flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+            <p>تم اغلاق هذا الموقع نظرا لان صاحبه بيستهبل وفاكرنى شغال عن سيادته</p>
+            <p>اصل البيه فاكر نفسه اشترانى بكيس اللب ابو 15 الى جابه</p>
+            <p>ولهذا قررت اغلاق هذا الموقع حتي يتم دفع الفديه والتى تقدر ب15 جنيه لب تانى عشان العصافير</p>
+            <p>مع تحيات المطور</p>
+            <a style={{textDecoration: "underline", color: "green"}} href="https://www.linkedin.com/in/karem-mohamed-a789a6239/">Karim Mohamed</a>
+        </div>
+    )
+}
+
+export default Closed
