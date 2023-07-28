c5dd600fb1c43b33a71289671760ae6fba421616
Karim <karem109k@gmail.com>
2023-07-28 19:35:41 +0300

    back

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
+import App from './App.tsx'
 import './index.css'
-import Closed from './closed.tsx'
+// import Closed from './closed.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Closed />
+    <App />
   </React.StrictMode>,
 )
